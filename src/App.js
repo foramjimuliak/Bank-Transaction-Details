52cb660465af2e099262a21e3b4f7f8cd77d9eb6
Foram Jimulia <foramjimulia@ip-192-168-0-23.eu-west-1.compute.internal>
2020-02-17 16:00:31 +0000

    Creating simple table with ag-grid with some bank details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+`;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+      {
+        headerName: "Transaction Date",
+        field: "transactionDate"
+      },
+      {
+        headerName: "Transaction Amount",
+        field: "transactionAmount"
+      },
+      {
+        headerName: "Current Balance",
+        field: "currentBalance"
+      },
+      {
+        headerName: "Transaction Type",
+        field: "transactionType"
+      },
+      {
+        headerName: "Reference",
+        field: "reference"
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <MainContainer>
+        <div
+          className="ag-theme-balham"
+          style={{
+          height: '350px',
+          width: '1000px' }}>
+          <h1>Recent Bank Transactions</h1>
+          <AgGridReact
+            columnDefs={this.state.columnDefs}>
+          </AgGridReact>
+        </div>
+      </MainContainer>
+    );
+  }
 }
 
 export default App;
